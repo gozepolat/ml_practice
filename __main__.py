d98b26ede3d3018eaf0f72121f067299465397d6
A. G. Polat <agplt@hotmail.com>
2016-03-28 14:17:05 +0300

    cnn lstm  on  twitter

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn import preprocessing
 from dl import models
 from dl import benchmark
-# from sklearn import svm  # future work: fusion
+from sklearn import svm  # future work: fusion
 import cPickle
 
 from prep import dataset
@@ -60,6 +60,9 @@ if __name__ == "__main__":
         pre_model.load_weights("data/pos_neg_model.hdf5")  # TODO uncomment
 
     print("finished loading the pretrained embedding layer weights")
+
+
+    # scores without pretrained weights
     # stratified cross-validation 10-fold as default, give n_fold=k to benchmark.cross_validate to change it
     evaluate = False  # do not calculate validation score in keras since it will be done extensively later
     lb = preprocessing.LabelBinarizer()
@@ -69,18 +72,32 @@ if __name__ == "__main__":
     lb.fit(neg_labels)
     neg_y_labels = benchmark.remap(np.concatenate((neg_y_train, neg_y_test)), neg_labels)
     neg_X = np.concatenate((neg_X_train, neg_X_test))
-    benchmark.cross_validate(len(neg_labels), neg_X, neg_y_labels, 5, "neg", neg_labels, max_words=max_words_in_sentence)
+    avg_scores = np.zeros((4, 5))
+    benchmark.cross_validate(len(neg_labels), neg_X, neg_y_labels, 10, "neg", neg_labels, avg_scores,
+                             max_words=max_words_in_sentence)
+    print("end of cross-validation for negative task")
+    print("average precision, recall, fscore and support values for each class:")
+    print(", ".join(neg_labels))
+    print(avg_scores / 10.0)
     # pos model
     print("starting cross-validation for pos model")
     pos_labels = ["Joy", "Desire", "Love", "Other"]
     lb = None
     lb = preprocessing.LabelBinarizer(sparse_output=True)
     lb.fit(pos_labels)
-    pos_y = lb.inverse_transform(np.concatenate((pos_y_train, pos_y_test)))
+    # pos_y = lb.inverse_transform(np.concatenate((pos_y_train, pos_y_test)))
+    pos_y_labels = benchmark.remap(np.concatenate((pos_y_train, pos_y_test)), pos_labels)
     pos_X = np.concatenate((pos_X_train, pos_X_test))
-    benchmark.cross_validate(len(pos_labels), pos_X, pos_y, 5, "pos", pos_labels, max_words=max_words_in_sentence)
+    avg_scores = np.zeros((4, 4))
+    benchmark.cross_validate(len(pos_labels), pos_X, pos_y_labels, 10, "pos", pos_labels, avg_scores,
+                             max_words=max_words_in_sentence)
+    print("end of cross-validation for positive task")
+    print("average precision, recall, fscore and support values for each class:")
+    print(", ".join(pos_labels))
+    print(avg_scores / 10.0)
 
-    """
+    #""" # Bonus: scores with pretrained weights, check this scores later, pretraining should make it better
+    print("scores with pretrained weights:")
     # train the dataset on a cnn / lstm architecture
     pos_model = models.construct_cnn_lstm(nb_class=4, stateful=False, convolutional=True,
                                           max_words=max_words_in_sentence,
@@ -94,6 +111,6 @@ if __name__ == "__main__":
                                           max_words=max_words_in_sentence,
                                           pretrained_embedding=pre_model.layers[0])
     score, acc = models.train_model(neg_model, neg_X_train, neg_X_test, neg_y_train, neg_y_test, nb_epoch=3)
+    print("negative model is trained with validation loss and accuracy", (score, acc))
+    #"""
 
-    # use the same embedding layer for neg?
-    print("negative model is trained with validation loss and accuracy", (score, acc))"""
--- dl/benchmark.py
+++ dl/benchmark.py
@@ -6,24 +6,24 @@ import cPickle
 import numpy as np
 
 
-def cross_validate(nb_class, X, y, nb_epoch, task, labels, n_folds=10, evaluate=False, max_words=100,
+def cross_validate(nb_class, X, y, nb_epoch, task, labels, avg_scores, n_folds=10, evaluate=False, max_words=100,
                    stateful=False, convolutional=True):
     skf = StratifiedKFold(y, n_folds=n_folds, shuffle=False, random_state=None)  # already shuffled
     for i, (train_ix, test_ix) in enumerate(skf):
         print ("Cross-validation fold: %d/%d" % (i + 1, n_folds))
-        pos_model = None  # Clearing the NN.
-        pos_model = models.construct_cnn_lstm(stateful=stateful, convolutional=convolutional, nb_class=nb_class,
-                                              max_words=max_words)
+        model = None  # Clearing the NN.
+        model = models.construct_cnn_lstm(stateful=stateful, convolutional=convolutional, nb_class=nb_class,
+                                          max_words=max_words)
         # pretrained_embedding=pre_model.layers[0])
         X_train, X_test = X[train_ix], X[test_ix]
         y_train, y_test = y[train_ix], y[test_ix]
-        models.train_model(pos_model, X_train, preprocessing.label_binarize(y_train, classes=labels),
+        models.train_model(model, X_train, preprocessing.label_binarize(y_train, classes=labels),
                            nb_epoch=nb_epoch,
                            evaluate=evaluate, max_words=max_words)
         if evaluate is False:
             X_test = models.pad(X_test, max_words=max_words)
-        y_test_pred = pos_model.predict(X_test)
-        y_test_pred = remap(y_test_pred, labels)
+        y_test_pred = model.predict_classes(X_test)
+        y_test_pred = [labels[i] for i in y_test_pred]
         cm = confusion_matrix(y_test, y_test_pred, labels=labels)
         print(", ".join(labels))
         print("confusion matrix:")
@@ -31,17 +31,19 @@ def cross_validate(nb_class, X, y, nb_epoch, task, labels, n_folds=10, evaluate=
         scores = precision_recall_fscore_support(y_test, y_test_pred, average=None, labels=labels)
         print("precision, recall, fscore and support values for each class:")
         print(", ".join(labels))
-        for i, label in enumerate(["precision", "recall", "fscore", "support"]):
-            print(label, scores[i])
+        for x, label in enumerate(["precision", "recall", "fscore", "support"]):
+            print(label, scores[x])
+            for j, k in enumerate(scores[x]):
+                avg_scores[x][j] += k
         print(", ".join(labels))
         cPickle.dump(cm, open("data/scores/%s_cm_cross_%d.pkl" % (task, i), "w"))
         cPickle.dump(cm, open("data/scores/%s_precision_recall_fscore_support_pos_cross_%d.pkl" % (task, i), "w"))
 
 
 def remap(arr, labels):
-        new_arr = np.array(["-------"]*len(arr))
-        for ix, k in enumerate(arr):
-            for i, j in enumerate(k):
-                if j == 1:
-                    new_arr[ix] = labels[i]
-        return new_arr
+    new_arr = np.array(["-------"] * len(arr))
+    for ix, k in enumerate(arr):
+        for i, j in enumerate(k):
+            if j == 1:
+                new_arr[ix] = labels[i]
+    return new_arr
